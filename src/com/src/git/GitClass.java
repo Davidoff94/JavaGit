e3a4ae50dada65ea17a160837448d7bb485eb0c6
David Hollo <hollodavidstefan@gmail.com>
2018-10-22 21:20:56 +0300

    qweqweqw

--- src/com/src/git/GitClass.java
+++ src/com/src/git/GitClass.java
@@ -7,18 +7,16 @@ public class GitClass {
 	}
 	
 	public static void main(String[] args) {
-		ClassC classC = new ClassC();
-		ClassC classC1 = new ClassC();
-		//commit 1
-		//commi 2
-		//commit 3
-		//commit 4
-		//commit 5
-		//commit 7
-		///coomiut 8
-		// TODO Auto-generated method stub
-		//hello all
-		//hello 
+		//blalblalsdlasldalsdlasldasldl
+		/*asd,as
+		dasdas
+		das
+		d
+		sad
+		sa
+		das
+		dsa
+		d*/
 	}
 
 }
